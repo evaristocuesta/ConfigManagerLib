d686a02d7a8da618a66c1b65544405b68d833494
evaristocuesta <evaristocg@gmail.com>
2020-11-01 19:37:41 +0100

    Add project files.

---
+++ ConfigManager.cs
@@ -0,0 +1,39 @@
+﻿using SerializerLib;
+using System.IO;
+
+namespace ConfigManagerLib
+{
+    public class ConfigManager<T> : IConfigManager<T> where T : new()
+    {
+        private ISerializer _serializer;
+
+        public ConfigManager(string configPath)
+        {
+            _serializer = new XMLSerializer();
+            ConfigPath = configPath;
+            Config = _serializer.Deserialize<T>(File.ReadAllText(ConfigPath));
+        }
+
+        /// <summary>
+        /// Config object readed from a file
+        /// </summary>
+        public T Config { get; private set; }
+
+        /// <summary>
+        /// Config file path 
+        /// </summary>
+        public string ConfigPath { get; private set; }
+
+        /// <summary>
+        /// Save the current config object in the ConfigPath file
+        /// </summary>
+        public void Save()
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(ConfigPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(ConfigPath));
+            }
+            File.WriteAllText(ConfigPath, _serializer.Serialize(Config));
+        }
+    }
+}
---
+++ IConfigManager.cs
@@ -0,0 +1,20 @@
+﻿namespace ConfigManagerLib
+{
+    public interface IConfigManager<T>
+    {
+        /// <summary>
+        /// Config object readed from a file
+        /// </summary>
+        T Config { get; }
+
+        /// <summary>
+        /// Config file path 
+        /// </summary>
+        string ConfigPath { get; }
+
+        /// <summary>
+        /// Save the current config object in the ConfigPath file
+        /// </summary>
+        void Save();
+    }
+}
